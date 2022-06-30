d1cd758d2b2a8cdf3a06afcd144dc9824473e774
TJ Mahr <tjmahr@users.noreply.github.com>
2022-06-30 09:52:20 -0500

    try to strip off readr info from data

--- data-raw/example-intelligibility.R
+++ data-raw/example-intelligibility.R
@@ -3,5 +3,7 @@
 data_fake_intelligibility <- readr::read_csv(
   "data-raw/example-intelligibility.csv",
   col_types = "in"
-)
+) |>
+  tibble::as_tibble()
+
 usethis::use_data(data_fake_intelligibility, overwrite = TRUE)
--- data-raw/example-rates.R
+++ data-raw/example-rates.R
@@ -3,5 +3,7 @@
 data_fake_rates <- readr::read_csv(
   "data-raw/example-rates.csv",
   col_types = "in"
-)
+) |>
+  tibble::as_tibble()
+
 usethis::use_data(data_fake_rates, overwrite = TRUE)
