4604fb0c89db2cfd32e78f4bc24b210b31081667
TJ Mahr <tjmahr@users.noreply.github.com>
2022-07-07 09:57:49 -0500

    computing chronological age

--- R/utils.R
+++ R/utils.R
@@ -21,3 +21,108 @@ format_year_month_age <- function(x) {
   months <- x %% 12L
   paste0(years, ";", months)
 }
+
+
+
+
+#' Compute chronological age in months
+#'
+#' Ages are rounded down to the nearest month. A difference of 20 months, 29
+#' days is interpreted as 20 months.
+#'
+#' @param t1,t2 dates in "yyyy-mm-dd" format
+#' @return the chronological ages in months. NA is returned if the age cannot be
+#'   computed.
+#' @export
+#' @examples
+#' # Two years exactly
+#' chrono_age("2014-01-20", "2012-01-20")
+#' #> 24
+#'
+#' # Shift a year
+#' chrono_age("2014-01-20", "2013-01-20")
+#' #> 12
+#' chrono_age("2014-01-20", "2011-01-20")
+#' #> 36
+#'
+#' # Shift a month
+#' chrono_age("2014-01-20", "2012-02-20")
+#' #> 23
+#' chrono_age("2014-01-20", "2011-12-20")
+#' #> 25
+#'
+#' # 3 months exactly
+#' chrono_age("2014-05-10", "2014-02-10")
+#' #> 3
+#'
+#' # Borrow a month when the earlier date has a later day
+#' chrono_age("2014-05-10", "2014-02-11")
+#' #> 2, equal to 2 months, 29 days rounded down to nearest month
+#'
+#' # Inverted argument order
+#' chrono_age("2012-01-20", "2014-01-20")
+#' #> 24
+#'
+#' # Multiple dates
+#' t1 <- c("2012-01-20", "2014-02-10", "2010-10-10")
+#' t2 <- c("2014-01-20", "2014-05-10", "2014-11-10")
+#' chrono_age(t1, t2)
+#' #> [1] 24  3 49
+chrono_age <- function(t1, t2) {
+  stopifnot(length(t1) == length(t2))
+  purrr::map2_dbl(t1, t2, purrr::possibly(chrono_age_single, NA))
+}
+
+#' Compute difference between two dates in months
+#' @noRd
+chrono_age_single <- function(t1, t2) {
+  difference <- diff_date(t1, t2)
+  12 * difference$y  + difference$m
+}
+
+#' Compute the difference between two dates
+#' @noRd
+diff_date <- function(t1, t2) {
+  stopifnot(length(t1) == 1, length(t2) == 1)
+
+  if (is.na(t1) || is.na(t2)) {
+    warning("Missing date: t1 = ", t1, ", t2 = ", t2, call. = FALSE)
+    return(list(y = NA, m = NA, d = NA))
+  }
+
+  t1 <- as.Date(t1)
+  t2 <- as.Date(t2)
+
+  # Sort dates and convert to a list
+  d1 <- as_date_list(min(t1, t2))
+  d2 <- as_date_list(max(t1, t2))
+
+  # Borrow a month
+  if (d2$d < d1$d) {
+    d2$m <- d2$m - 1
+    d2$d <- d2$d + 30
+  }
+
+  # Borrow a year
+  if (d2$m < d1$m) {
+    d2$y <- d2$y - 1
+    d2$m <- d2$m + 12
+  }
+
+  diff <- list(
+    y = d2$y - d1$y,
+    m = d2$m - d1$m,
+    d = d2$d - d1$d
+  )
+  diff
+}
+
+# A lightweight data structure for hand-manipulating dates
+as_date_list <- function(date) {
+  date <- as.Date(date)
+  y <- date |> format("%Y") |> as.numeric()
+  m <- date |> format("%m") |> as.numeric()
+  d <- date |> format("%d") |> as.numeric()
+
+  list(y = y, m = m, d = d)
+}
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,3 +3,40 @@ test_that("Age formatting works", {
     format_year_month_age() |>
     expect_equal(c("0;1", "1;1", "1;11", "2;0", "NA;NA"))
 })
+
+
+test_that("Chronological age in months", {
+  # Two years exactly
+  expect_equal(chrono_age("2014-01-20", "2012-01-20"), 24)
+
+  # Shift a year
+  expect_equal(chrono_age("2014-01-20", "2013-01-20"), 12)
+  expect_equal(chrono_age("2014-01-20", "2011-01-20"), 36)
+
+  # Shift a month
+  expect_equal(chrono_age("2014-01-20", "2012-02-20"), 23)
+  expect_equal(chrono_age("2014-01-20", "2011-12-20"), 25)
+
+  # Multiple dates
+  t1 <- c("2014-01-20", "2014-01-20")
+  t2 <- c("2012-02-20", "2011-12-20")
+  expect_equal(chrono_age(t1, t2), c(23, 25))
+
+  # Missing dates raise warnings and default to NA
+  expect_warning(chrono_age(NA, "2011-12-20"))
+  expect_true(is.na(suppressWarnings(chrono_age(NA, "2011-12-20"))))
+
+  # 3 months exactly
+  expect_equal(chrono_age("2014-05-10", "2014-02-10"), 3)
+
+  # Borrow a month when the earlier date has a later day
+  expect_equal(chrono_age("2014-05-10", "2014-02-11"), 2)
+
+  # Reversed arguments
+  expect_equal(chrono_age("2012-01-20", "2014-01-20"), 24)
+
+  # Checks against EVT/PPVT examples
+  expect_equal(chrono_age("2007-06-29", "2001-03-03"),  (6 * 12) + 3)
+  expect_equal(chrono_age("2007-05-08", "1999-08-02"),  (7 * 12) + 9)
+  expect_equal(chrono_age("2007-08-13", "1985-11-24"), (21 * 12) + 8)
+})
